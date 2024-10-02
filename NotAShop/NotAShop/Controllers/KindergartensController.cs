cb8ee4c82166ed72c919d8290ff81a784a3f0b47
AliikaPSP <aliika.puusepp@hotmail.com>
2024-10-02 18:20:27 +0300

    Kindergartens controller

---
+++ NotAShop/NotAShop/Controllers/KindergartensController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NotAShop.Controllers
+{
+    public class KindergartensController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
