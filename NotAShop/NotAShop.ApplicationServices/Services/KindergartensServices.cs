3f57e0635119086a85bb1c5cc0eeeb5729d1a857
AliikaPSP <aliika.puusepp@hotmail.com>
2024-10-04 13:13:29 +0300

    KindergartenServices

--- NotAShop/NotAShop.ApplicationServices/Services/KindergartensServices.cs
+++ NotAShop/NotAShop.ApplicationServices/Services/KindergartensServices.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using NotAShop.Core.Domain;
+using NotAShop.Core.ServiceInterface;
+using NotAShop.Data;
 
 namespace NotAShop.ApplicationServices.Services
 {
-    public class KindergartensServices
+    public class KindergartensServices : IKindergartensServices
     {
+        private readonly NotAShopContext _context;
+
+        public KindergartensServices
+            (
+                NotAShopContext context
+            )
+        {
+            _context = context;
+        }
+
+        public async Task<Kindergarten> DetailAsync(Guid id)
+        {
+            var result = await _context.Kindergartens
+                .FirstOrDefaultAsync(x => x.Id == id);
+            return result;
+        }
     }
 }
