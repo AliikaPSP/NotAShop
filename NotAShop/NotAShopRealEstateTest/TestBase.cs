d080767e2680c1713255c4a468e66942e9114121
Aliika Puusepp <153951390+AliikaPSP@users.noreply.github.com>
2024-10-15 15:05:45 +0300

    RealEstatexUnitTestBase

---
+++ NotAShop/NotAShopRealEstateTest/TestBase.cs
@@ -0,0 +1,30 @@
+﻿
+
+using Microsoft.Extensions.DependencyInjection;
+using System.Security.Authentication.ExtendedProtection;
+
+namespace NotAShopRealEstateTest
+{
+
+    public abstract class TestBase
+    {
+        protected IServiceProvider serviceProvider { get; set; }
+
+        protected TestBase() 
+        {
+            var services = new ServiceCollection();
+            SetupServices(services);
+            serviceProvider = services.BuildServiceProvider();
+        }
+
+        protected T Svc<T>()
+        {
+            return serviceProvider.GetService<T>();
+        }
+
+        public virtual void SetupServices(IServiceCollection services)
+        {
+
+        }
+    }
+}
