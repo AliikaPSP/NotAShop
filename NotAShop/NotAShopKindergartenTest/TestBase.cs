a58bdfa84f2d276cc6cbbb196a9748d775d82b18
Aliika Puusepp <153951390+AliikaPSP@users.noreply.github.com>
2024-10-26 20:24:59 +0300

    NotAShop Kindergarten TestBase & Dependencies

---
+++ NotAShop/NotAShopKindergartenTest/TestBase.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NotAShop.ApplicationServices.Services;
+using NotAShop.Core.ServiceInterface;
+using NotAShop.Data;
+using System.Security.Authentication.ExtendedProtection;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+
+namespace NotAShopKindergartenTest
+{
+    public abstract class TestBase
+    {
+    }
+}
