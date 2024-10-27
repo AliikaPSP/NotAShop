363755a536b0371c05194f8ec843d5ebc2324636
Aliika Puusepp <153951390+AliikaPSP@users.noreply.github.com>
2024-10-27 18:12:21 +0200

    Imacros & IHost, First test try

--- NotAShop/NotAShopKindergartenTest/KindergartenTest.cs
+++ NotAShop/NotAShopKindergartenTest/KindergartenTest.cs
@@ -1,11 +1,33 @@
+using Microsoft.AspNetCore.Http;
+using NotAShop.Core.Domain;
+using NotAShop.Core.Dto;
+using NotAShop.Core.ServiceInterface;
+using NotAShop.Data;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
 namespace NotAShopKindergartenTest
 {
-    public class UnitTest1
+    public class KindergartenTest : TestBase
     {
         [Fact]
-        public void Test1()
+        public async Task ShouldNot_AddKindergarten_WhenReturnResult()
         {
+            //Arrange
+            KindergartenDto dto = new();
 
+            dto.GroupName = "Kiisud";
+            dto.ChildrenCount = 5;
+            dto.KindergartenName = "Kiisuaed";
+            dto.Teacher = "Mrs. Kiisu";
+            dto.CreatedAt = DateTime.Now;
+            dto.UpdatedAt = DateTime.Now;
+
+            //Act
+            var result = await Svc<IKindergartensServices>().Create(dto);
+
+            //Assert
+
+            Assert.NotNull(result);
         }
     }
 }
--- NotAShop/NotAShopKindergartenTest/TestBase.cs
+++ NotAShop/NotAShopKindergartenTest/TestBase.cs
@@ -6,10 +6,59 @@ using System.Security.Authentication.ExtendedProtection;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Hosting;
+using NotAShopKindergartenTest.Macros;
+using NotAShopKindergartenTest.Mock;
 
 namespace NotAShopKindergartenTest
 {
     public abstract class TestBase
     {
+        protected IServiceProvider serviceProvider { get; set; }
+
+        protected TestBase()
+        {
+            var services = new ServiceCollection();
+            SetupServices(services);
+            serviceProvider = services.BuildServiceProvider();
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        protected T Svc<T>()
+        {
+            return serviceProvider.GetService<T>();
+        }
+
+        public virtual void SetupServices(IServiceCollection services)
+        {
+            services.AddScoped<IKindergartensServices, KindergartensServices>();
+            services.AddScoped<IFileServices, FileServices>();
+            services.AddScoped<IHostEnvironment, MockIHostEnvironment>();
+
+            services.AddDbContext<NotAShopContext>(x =>
+            {
+                x.UseInMemoryDatabase("TEST");
+                x.ConfigureWarnings(e => e.Ignore(InMemoryEventId.TransactionIgnoredWarning));
+            });
+
+            RegisterMacros(services);
+        }
+
+        private void RegisterMacros(IServiceCollection services)
+        {
+            var macroBaseType = typeof(IMacros);
+
+            var macros = macroBaseType.Assembly.GetTypes()
+                .Where(x => macroBaseType.IsAssignableFrom(x) && !x.IsInterface
+                && !x.IsAbstract);
+
+            foreach (var macro in macros)
+            {
+                services.AddSingleton(macro);
+            }
+        }
     }
 }
