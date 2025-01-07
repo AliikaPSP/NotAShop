dbc5b49845f8c83dbc24149c5252ecdbd9eb4629
Aliika Puusepp <153951390+AliikaPSP@users.noreply.github.com>
2025-01-07 17:03:46 +0200

    SignalR UserHub & Count

--- NotAShop/NotAShop/Program.cs
+++ NotAShop/NotAShop/Program.cs
@@ -99,6 +99,7 @@ namespace NotAShop
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
             app.MapHub<ChatHub>("/chatHub");
+            app.MapHub<UserHub>("/hubs/userCount");
 
             app.Run();
         }
